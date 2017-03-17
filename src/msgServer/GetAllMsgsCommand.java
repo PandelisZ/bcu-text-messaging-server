25322b0b39f0d411309e1a98308c9466627dc1b9
Jason <jasonfrancis1997@hotmail.com>
2017-03-17 13:54:46 +0000

    Added getAllMessages (106) command.
    Removed main.java since it wasn't really needed.
    
    Added relative path for pwd.txt

--- src/msgServer/GetAllMsgsCommand.java
+++ src/msgServer/GetAllMsgsCommand.java
@@ -17,24 +17,41 @@ public class GetAllMsgsCommand implements Command {
 
     public void execute() throws IOException {
         //declare a variable user of type String and use it to get the user from the inputstream
-
+        String user = in.readLine();
         //check if current user is not equal to null and current user is equal to the user (use the method getCurrentUser())
-
-        //intialise an array (msgs) that is used to hold all the messages read and set it's initialised value to null
-
-        //use the method getAllMessages(user) to populate the msgs array
-
-        //check if msgs is not equal to null
-
-        //write to the OutputStream the message status code as specified in the protocol
-
-        //write the length of the messages returned
-
-        //Loop through the messages and write the sender, date and cotent to the outputstream (use provided methods)
-
-        //Flush the outputstream
+        if(user != null && user.equals(conn.getCurrentUser())) {
+            //intialise an array (msgs) that is used to hold all the messages read and set it's initialised value to null
+            Message[] msgs = null;
+            //use the method getAllMessages(user) to populate the msgs array
+            MessageCollection messages = conn.getServer().getMessages();
+            msgs = messages.getAllMessages(user);
+            //check if msgs is not equal to null
+            if(msgs != null){
+                //write to the OutputStream the message status code as specified in the protocol
+                out.write("201" + "\r\n");
+                //write the length of the messages returned
+                out.write(msgs.length + "\r\n");
+                //Loop through the messages and write the sender, date and cotent to the outputstream (use provided methods)
+                for (Message m : msgs){
+                    out.write(m.getSender() + "\r\n");
+                    out.write(m.getDate() + "\r\n");
+                    out.write(m.getContent() + "\r\n");
+                }
+                //Flush the outputstream
+                out.flush();
+            }else{
+                new ErrorCommand(in, out, conn, "No messages");
+            }
+        }else{
+            new ErrorCommand(in, out, conn, "You are not logged on");
+        }
 
         //capture adequet errors (No messages) or (You are not logged on)
+
+
+
+
+
 
     }
 }
