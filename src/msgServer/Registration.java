bf0dd217542a4dbe70691f0cb5896a16d019da04
Matas <matas.vireikis@hotmail.com>
2017-04-22 16:23:38 +0100

    Write to database, tested, works.
    Errors also tested, they appear when they should

--- src/msgServer/Registration.java
+++ src/msgServer/Registration.java
@@ -22,7 +22,10 @@ public class Registration implements Command {
         this.conn = serverConn;
     }
    public void execute() throws IOException {
-
+/**
+ * the code below records user inputs when attempting to register
+ * these can then be used by the sql statement to make additions to the database
+ */
         String user = in.readLine();
         String password = in.readLine();
         String dob = in.readLine();
@@ -30,25 +33,43 @@ public class Registration implements Command {
         String add = in.readLine();
         String sqlInsertUser;
 
-       FileWriter fileOut = new FileWriter("TestFile1.txt", true);
-       fileOut.write(user);
-       fileOut.write(password);
-       fileOut.write(dob);
-       fileOut.write(tel);
-       fileOut.write(add);
-
-       fileOut.close();
 
 
+       if(conn.getCurrentUser() == null) {
+
+/**
+
+@param sqlnstertUser
+ this code is used to add a user into the database if it does not already exist
+**/
+           if(conn.getServer().isValidUser(user) == false) {
+               sqlInsertUser = "INSERT INTO `users` (`user`, `pass`, `dob`, `tel`, `add`) VALUES ('" + user + "','" + password + "', STR_TO_DATE('" + dob + "', '%Y-%m-%d'),'" + tel + "','" + add + "')";
+               conn.getServer().getDatabase().executeSQLUpdate(sqlInsertUser);
+
+               out.write("200\r\n");
+               out.flush();
+           }
+
+           /**
+            * The code below ensures that multiple users with identical usernames cannot be added to the database
+            * An error is thrown if an attempt is made to do this
+            */
+           else {
+               (new ErrorCommand(in, out, conn, "User already with that name already exists")).execute();
+
+           }
+           }
+       /**
+        * An error will be thrown if a user attempts to access the registration protocol while already logged into an account
+        */
+       else {
+           (new ErrorCommand(in, out, conn, "You cannot register while logged into an account.")).execute();
 
 
-       if(conn.getServer().isValidUser(user) == false) {
-           sqlInsertUser =  "INSERT INTO users (user, pass, dob, tel, add) VALUES ('"+user+"','"+password+"', STR_TO_DATE('+dob+', '%Y-%m-%d'),'"+tel+"','"+add+"')";
-           conn.getServer().getDatabase().executeSQLUpdate(sqlInsertUser);
        }
-
-       out.write("200\r\n");
-       out.flush();
-
-    }
+   }
 }
+
+
+
+
